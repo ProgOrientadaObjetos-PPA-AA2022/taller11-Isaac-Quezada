114a9a57ef9e02932b289ca1e0ea8ee6d82c8c87
Isaac-Quezada <imquezada1@utpl.edu.ec>
2022-07-01 16:45:08 -0500

    Tarea en clase 01/07/2022

---
+++ Taller11/src/paquete03/Cuenta.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete03;
+
+/**
+ *
+ * @author Matias Quezada
+ */
+public class Cuenta {
+    
+    public String nombreCliente;
+    
+    public double valorCancelar;
+    public  double subtotal;
+    public double iva ;
+}
